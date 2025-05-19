c72bf9cdafedf54aae7b7a17210112953761dfe1
Kéven Patrício <patriciokeven4@gmail.com>
2025-05-19 14:34:16 -0300

    Atualizando no codigo e gitignore

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.post('/webhook-sost', async (req, res) => {
     }
 });
 
-const PORT = 3000
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
